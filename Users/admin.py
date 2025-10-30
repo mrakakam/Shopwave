37459cad8a20fa82700fd3ac34806379fee8506c
mrakakam <johnakakamjohn@gmail.com>
2025-10-30 03:58:43 -0700

    any commit message

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin #for handling user management
+from .models import User, SellerProfile, Address
 
-# Register your models here.
+
+@admin.register(User) #register the User model with the admin site
+class CustmerAdmin(UserAdmin):
+    model = User
+    List_display = ('email', 'first_name', 'last_name', 'is_seller', 'is_buyer', 'is_staff',  'is_active')
+    list_filter = ('is_seller', 'is_buyer', 'is_staff', 'is_active')
+    fieldsets = (
+
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_seller', 'is_buyer', 'is_staff', 'is_active' 'phone_number')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_seller', 'is_buyer', 'is_staff', 'is_active')}
+        ),
+    )
+
+
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+    @admin.register(SellerProfile)
+    class SellerProfileAdmin(admin.ModelAdmin):
+        List_display = ('user', 'business_name', 'verified', 'kyc_status', 'created_at')
+        search_fields = ('user_email', 'business_name')
+        list_filter = ('verified', 'kyc_status')
+        readonly_fields = ('created_at', 'updated_at')
--- Users/models.py
+++ Users/models.py
@@ -67,4 +67,40 @@ kyc_status = models.CharField(
     def __str__(self):
         return f" {self.user.email} - {self.business_name}"
 
-    
+    class  Address(models.Model):
+        ADDReSS_TYPE_CHOICES = [
+        ('shipping', 'Shipping'),
+        ('billing', 'Billing'),
+        ('both', 'Both'),
+    ]
+
+    user= models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='addresses'
+    )
+
+    address_type = models.CharField(
+        max_length=20,
+        choices=ADDReSS_TYPE_CHOICES,
+        default='shipping'
+    )
+
+    street_address = models.CharField(_('street address'), max_length=255)
+    city = models.CharField(_('city'), max_length=100)
+    state = models.CharField(_('state'), max_length=100)
+    country = models.CharField(_('country'), max_length=100)
+    postal_code = models.CharField(max_length=20)
+    is_default = models.BooleanField(default=False)
+    created_at = models.DateTimeField( auto_now_add=True)
+    updated_at = models.DateTimeField( auto_now=True)
+
+
+class Meta:
+        db_table = 'addresses'
+        vrbose_name_plural = 'Addresses'
+
+def  __str__(self):
+        return f"{self.user.email} - {self.address_type} Address"
+
+
